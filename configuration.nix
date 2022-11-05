f353fec38479c8bef050c515476607c58ef88b72
Brian Tipton <briantiptondev@gmail.com>
2022-11-05 13:13:37 -0500

    added mkJup alias, removed comments

--- configuration.nix
+++ configuration.nix
@@ -60,12 +60,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -216,6 +210,7 @@
       updatedb = "sudo updatedb";
       copy = "wl-copy";
       repl = "nix-shell $HOME/Developer/NixShells/IPython/shell.nix --command ipython";
+      mkJup = "docker run -v \"\${PWD}\":/home/jovyan/work -p 8888:8888 jupyter/datascience-notebook";
     };
     
     ohMyZsh = {
