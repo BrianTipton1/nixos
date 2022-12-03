ae20d0db14228aea33365350bb77cf30e74bd1e9
Brian Tipton <briantiptondev@gmail.com>
2022-12-03 16:57:58 -0600

    formatting

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,12 @@
-{ config, pkgs, ... }:
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+{ config, pkgs, ... }: {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  
 
   networking.hostName = "nixos"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -34,14 +31,14 @@
   boot.kernelParams = [ "nvidia-drm.modeset=1" ];
   hardware.nvidia.powerManagement.enable = true;
   ### End Nvidia Setup
-  
+
   # Xmonad
   services.xserver.windowManager.xmonad.enable = true;
 
   # KDE Plasma Setup
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-  
+
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
@@ -71,9 +68,9 @@
     packages = with pkgs; [
       #Browsers
       pkgs.firefox
-      
+
       # School
-      pkgs.anki-bin      
+      pkgs.anki-bin
       # Email
       pkgs.thunderbird
       pkgs.birdtray
@@ -89,17 +86,17 @@
 
       # Voice/Video Call      
       pkgs.zoom-us
-      
+
       # IDE's
       pkgs.jetbrains.pycharm-professional
       pkgs.jetbrains.idea-ultimate
       pkgs.jetbrains.rider
       pkgs.libsForQt5.kate
       pkgs.unstable.vscode
-      
+
       # Audio
       pkgs.rnnoise-plugin
-      
+
       # Screen Capture
       pkgs.obs-studio
 
@@ -129,40 +126,41 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = [
-      # Editors
-      pkgs.neovim
-      pkgs.sublime4
+    # Editors
+    pkgs.neovim
+    pkgs.sublime4
 
-      # Utilities
-      pkgs.wget
-      pkgs.util-linux
-      pkgs.pciutils
-      pkgs.bat
-      pkgs.exa
-      pkgs.fzf
-      pkgs.git
-      pkgs.killall
-      pkgs.clamav
-      pkgs.unzip
-      pkgs.sublime-merge
-      pkgs.wl-clipboard
-      pkgs.ripgrep      
-      pkgs.htop
-      pkgs.file
-      pkgs.github-desktop
-      pkgs.cachix
-      pkgs.direnv
+    # Utilities
+    pkgs.wget
+    pkgs.util-linux
+    pkgs.pciutils
+    pkgs.bat
+    pkgs.exa
+    pkgs.fzf
+    pkgs.git
+    pkgs.killall
+    pkgs.clamav
+    pkgs.unzip
+    pkgs.sublime-merge
+    pkgs.wl-clipboard
+    pkgs.ripgrep
+    pkgs.htop
+    pkgs.file
+    pkgs.github-desktop
+    pkgs.cachix
+    pkgs.direnv
 
-      # Compilers/Interpreters
-      pkgs.lua
-      pkgs.nodejs
+    # Compilers/Interpreters
+    pkgs.lua
+    pkgs.nodejs
 
-      # Global Language Servers
-      pkgs.unstable.nil
+    # Global Language Servers/ Formatters
+    pkgs.unstable.nil
+    pkgs.nixfmt
 
-      # Terminal Emulators
-      pkgs.kitty
-      pkgs.wezterm
+    # Terminal Emulators
+    pkgs.kitty
+    pkgs.wezterm
   ];
 
   # List services that you want to enable:
@@ -175,12 +173,12 @@
     enable = true;
     localuser = null;
     locate = pkgs.mlocate;
-    interval = "hourly"; 
-    };
-  
+    interval = "hourly";
+  };
+
   # Clamav/Freshclam Service
-  services.clamav.daemon.enable = true; 
-  services.clamav.updater.enable = true; 
+  services.clamav.daemon.enable = true;
+  services.clamav.updater.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -189,16 +187,16 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-  
+
   ## Default Shell to zsh
   #users.defaultUserShell = pkgs.zsh;
   # Aliases and Plugins
   programs.zsh = {
-    
+
     enable = true;
     syntaxHighlighting.enable = true;
-    autosuggestions.enable = true;   
-    
+    autosuggestions.enable = true;
+
     ## Aliases
     shellAliases = {
       vim = "nvim";
@@ -213,45 +211,47 @@
       nuke = "rm -rf";
       update = "sudo nixos-rebuild switch";
       upgrade = "sudo nixos-rebuild switch --upgrade";
-      sysadmin = "sudo cp $HOME/Developer/nixos/configuration.nix /etc/nixos/configuration.nix && sudo cp $HOME/Developer/nixos/flake.nix /etc/nixos/flake.nix && sudo nixos-rebuild switch";
+      sysadmin =
+        "sudo cp $HOME/Developer/nixos/configuration.nix /etc/nixos/configuration.nix && sudo cp $HOME/Developer/nixos/flake.nix /etc/nixos/flake.nix && sudo nixos-rebuild switch";
       sysedit = "cd $HOME/Developer/nixos/ && code .";
       updatedb = "sudo updatedb";
       copy = "xclip -selection c";
-      ipython = "nix-shell $HOME/Developer/NixShells/IPython/shell.nix --command ipython";
-      mkJup = "docker run -v \"\${PWD}\":/home/jovyan/work -p 8888:8888 jupyter/datascience-notebook";
-      nix-editor = "nix-shell --command \"code $PWD; return\"";
-      repl = "nix-shell $HOME/Developer/NixShells/ghci/shell.nix --command ghci";
+      ipython =
+        "nix-shell $HOME/Developer/NixShells/IPython/shell.nix --command ipython";
+      mkJup = ''
+        docker run -v "''${PWD}":/home/jovyan/work -p 8888:8888 jupyter/datascience-notebook'';
+      nix-editor = ''nix-shell --command "code $PWD; return"'';
+      repl =
+        "nix-shell $HOME/Developer/NixShells/ghci/shell.nix --command ghci";
     };
-    
+
     ohMyZsh = {
       enable = true;
-      plugins = [ "git" "sudo" "fzf" "z"];
+      plugins = [ "git" "sudo" "fzf" "z" ];
       theme = "half-life";
     };
     enableGlobalCompInit = false;
 
-    promptInit = 
-    ''
-    mkMonad() {
-        if [ -z "$1" ]
-        then 
-          echo "No argument supplied for mkMonad"
-        else 
-          mkdir "$1" && cd "$1" && nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [ cabal-install ])" --run "cabal init" && cp $HOME/Developer/NixShells/HaskellBase/default.nix .
-        fi
-    }
+    promptInit = ''
+      mkMonad() {
+          if [ -z "$1" ]
+          then 
+            echo "No argument supplied for mkMonad"
+          else 
+            mkdir "$1" && cd "$1" && nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [ cabal-install ])" --run "cabal init" && cp $HOME/Developer/NixShells/HaskellBase/default.nix .
+          fi
+      }
     '';
 
-    loginShellInit = 
-    ''
-    export ZSHZ_DATA="$HOME/.cache/zsh/.z"
-    # Create a cache folder for zcompdump if it isn't exists
-    if [ ! -d "$HOME/.cache/zsh" ]; then
-        mkdir -p $HOME/.cache/zsh
-    fi
-    export ZSH_COMPDUMP="$HOME/.cache/zsh/zcompdump-$HOST-$ZSH_VERSION"
+    loginShellInit = ''
+      export ZSHZ_DATA="$HOME/.cache/zsh/.z"
+      # Create a cache folder for zcompdump if it isn't exists
+      if [ ! -d "$HOME/.cache/zsh" ]; then
+          mkdir -p $HOME/.cache/zsh
+      fi
+      export ZSH_COMPDUMP="$HOME/.cache/zsh/zcompdump-$HOST-$ZSH_VERSION"
     '';
-};
+  };
   ## End zsh
 
   ## Virtual Box Stuff
@@ -276,24 +276,20 @@
   # Steam/Game Related Settings.. Bluetooth
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall =
+      true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall =
+      true; # Open ports in the firewall for Source Dedicated Server
   };
   services.blueman.enable = true;
   hardware.bluetooth.enable = true;
-    nixpkgs.config.packageOverrides = pkgs: {
-    steam = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        libgdiplus
-      ];
-    };
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam =
+      pkgs.steam.override { extraPkgs = pkgs: with pkgs; [ libgdiplus ]; };
   };
   hardware.xpadneo.enable = true;
   ## End Steam
 
   # Fonts
-  fonts.fonts = with pkgs; [
-    jetbrains-mono
-    nerdfonts
-  ];
+  fonts.fonts = with pkgs; [ jetbrains-mono nerdfonts ];
 }
